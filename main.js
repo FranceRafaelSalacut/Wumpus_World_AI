320a132e412683215b9a719e77925692427d2594
FranceRafaelSalacut <fpsalacut@up.edu.ph>
2024-06-06 07:24:40 +0800

    can now display stench and breeze

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ function randomizeGrid(){
     gameGrid = new Object
     tempGrid = []
     for (let i = 1; i <= n * n; i++){
-       gameGrid[`tile${i}`] = {Player: 0, Pit: 0, Goblin: 0, breeze:0, stench:0}
+       gameGrid[`tile${i}`] = {Player: 0, Pit: 0, Goblin: 0, Breeze:0, Stench:0}
        tempGrid.push(i)
     }
 
@@ -61,6 +61,14 @@ function randomizeGrid(){
     tempGrid.splice(tempGrid.indexOf(goblinPos), 1)
 
     stench = [goblinPos-n*1, goblinPos-1, goblinPos+1, goblinPos+n*1]
+    for (let i = 0; i<stench.length; i++){
+        if(stench[i] > 0 && stench[i] <= n*n){
+            if(!(goblinPos%n==0 && goblinPos+1 == stench[i]) && !(goblinPos%n==1 && goblinPos-1 == stench[i])){
+                gameGrid[`tile${stench[i]}`].Stench = 1
+            }
+           
+        }
+    }
 
     numPit = n-3
 
@@ -68,8 +76,14 @@ function randomizeGrid(){
         index = (Math.floor(Math.random() * tempGrid.length))
         holePos = tempGrid[index]
         gameGrid[`tile${holePos}`].Pit = 1
-
         tempGrid.splice(tempGrid.indexOf(holePos), 1)
+
+        breeze= [holePos-n*1, holePos-1, holePos+1, holePos+n*1]
+        for (let i = 0; i<breeze.length; i++){
+            if(breeze[i] > 0 && breeze[i] <= n*n){
+                gameGrid[`tile${breeze[i]}`].Breeze = 1
+            }
+    }
 
     }
 
@@ -93,7 +107,14 @@ function renderGrid() {
         tile.style.height = `${tileSize}%`;
         if(gameGrid[`tile${i}`].Pit == 1){
             tile.style.backgroundImage = 'url("sprites/cave_pit.png")'
-        }else{
+        }else if(gameGrid[`tile${i}`].Stench == 1 && gameGrid[`tile${i}`].Breeze == 1 && gameGrid[`tile${i}`].Goblin == 0 && gameGrid[`tile${i}`].Player == 0){
+            tile.style.backgroundImage = 'url("sprites/cave_wall_stench_breeze.png")'
+        }else if(gameGrid[`tile${i}`].Stench == 1 && gameGrid[`tile${i}`].Goblin == 0 && gameGrid[`tile${i}`].Player == 0){
+            tile.style.backgroundImage = 'url("sprites/cave_wall_stench.png")'
+        }else if(gameGrid[`tile${i}`].Breeze == 1 && gameGrid[`tile${i}`].Goblin == 0 && gameGrid[`tile${i}`].Player == 0){
+            tile.style.backgroundImage = 'url("sprites/cave_wall_breeze.png")'
+        }
+        else{
             tile.style.backgroundImage = 'url("sprites/cave_wall.png")'
         }
         container.appendChild(tile);
