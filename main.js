5279d9553344d744ee64e9fafc3bb5ec26fee787
FranceRafaelSalacut <fpsalacut@up.edu.ph>
2024-06-06 14:05:44 +0800

    can now spawn an arrow. board can now be covered

--- main.js
+++ main.js
@@ -4,6 +4,43 @@ window.onload = function(){
 }
 var playerLocation
 var gameGrid
+var covered = false
+
+function revealGrid(){
+    console.log("Reveal thy labyrinth")
+    const n = document.getElementById("slider").value
+    for(let i = 1; i <= n*n; i++){
+        uncoverGrid(i)
+    }
+}
+
+function uncoverGrid(i){
+    const cont = document.getElementById(`tile${i}`)
+    const gGrd = gameGrid[`tile${i}`]
+    const bg = gGrd.backgroundImage
+
+    if(gGrd.revealed){
+        return 
+    }
+
+    gGrd.revealed = true
+
+    if(gGrd.Player == 1){
+        placeCharacter(i,1)
+    }
+        
+    if(gGrd.Goblin == 1){
+        placeCharacter(i,2)
+    }
+
+    if(gGrd.Arrow == 1){
+        placeCharacter(i,3)
+    }
+
+    cont.style.backgroundColor = "None"
+    cont.style.backgroundImage = bg
+
+}
 
 function placeCharacter(Player, type){
 
@@ -11,10 +48,15 @@ function placeCharacter(Player, type){
     const tile = document.createElement('img')
     if(type == 1){
         tile.setAttribute('src', `sprites/Goblin_Slayer/down.png`)
+        tile.setAttribute('id', `sprite`)
     }else if(type == 2){
         tile.setAttribute('src', `sprites/goblin.png`)
-    }
         tile.setAttribute('id', `sprite`)
+    }else if(type == 3){
+        tile.setAttribute('src', `sprites/Arrow.png`)
+        tile.setAttribute('id', `arrow`)
+    }
+        
     container.appendChild(tile)
 }
 
@@ -25,7 +67,7 @@ function randomizeGrid(){
     gameGrid = new Object
     tempGrid = []
     for (let i = 1; i <= n * n; i++){
-       gameGrid[`tile${i}`] = {Player: 0, Pit: 0, Goblin: 0, Breeze:0, Stench:0}
+       gameGrid[`tile${i}`] = {Player: 0, Pit: 0, Goblin: 0, Breeze:0, Stench:0, Arrow:0, backgroundImage:"", revealed:false}
        tempGrid.push(i)
     }
 
@@ -50,7 +92,7 @@ function randomizeGrid(){
         tempGrid.splice(tempGrid.indexOf(playerStart - 1), 1)
         tempGrid.splice(tempGrid.indexOf(playerStart - n*1), 1)
     }
-
+    
     index = (Math.floor(Math.random() * tempGrid.length))
     goblinPos = tempGrid[index]
 
@@ -62,6 +104,7 @@ function randomizeGrid(){
     for (let i = 0; i<stench.length; i++){
         if(stench[i] > 0 && stench[i] <= n*n){
             if(!(goblinPos%n==0 && goblinPos+1 == stench[i]) && !(goblinPos%n==1 && goblinPos-1 == stench[i])){
+                tempGrid.splice(tempGrid.indexOf(stench[i]), 1)
                 gameGrid[`tile${stench[i]}`].Stench = 1
             }
            
@@ -80,6 +123,7 @@ function randomizeGrid(){
         for (let i = 0; i<breeze.length; i++){
             if(breeze[i] > 0 && breeze[i] <= n*n){
                 if(!(holePos%n==0 && holePos+1 == breeze[i]) && !(holePos%n==1 && holePos-1 == breeze[i])){
+                    tempGrid.splice(tempGrid.indexOf(breeze[i]), 1)
                     gameGrid[`tile${breeze[i]}`].Breeze = 1
                 }
                
@@ -88,8 +132,13 @@ function randomizeGrid(){
 
     }
 
-    renderGrid()
+    index = (Math.floor(Math.random() * tempGrid.length))
+    arrowPos = tempGrid[index]
+    gameGrid[`tile${arrowPos}`].Arrow = 1
+    tempGrid.splice(tempGrid.indexOf(arrowPos), 1)
 
+    renderGrid()
+    revealGrid()
 }
 
 function renderGrid() {
@@ -104,30 +153,29 @@ function renderGrid() {
         const tile = document.createElement('div');
         tile.classList.add('tile')
         tile.setAttribute('id', `tile${i}`)
+        //tile.setAttribute('onmouseover', `uncoverGrid(${i})`)
         tile.style.width = `${tileSize}%`;
         tile.style.height = `${tileSize}%`;
-        if(gameGrid[`tile${i}`].Pit == 1){
-            tile.style.backgroundImage = 'url("sprites/cave_pit.png")'
-        }else if(gameGrid[`tile${i}`].Stench == 1 && gameGrid[`tile${i}`].Breeze == 1 && gameGrid[`tile${i}`].Goblin == 0 && gameGrid[`tile${i}`].Player == 0){
-            tile.style.backgroundImage = 'url("sprites/cave_wall_stench_breeze.png")'
-        }else if(gameGrid[`tile${i}`].Stench == 1 && gameGrid[`tile${i}`].Goblin == 0 && gameGrid[`tile${i}`].Player == 0){
-            tile.style.backgroundImage = 'url("sprites/cave_wall_stench.png")'
-        }else if(gameGrid[`tile${i}`].Breeze == 1 && gameGrid[`tile${i}`].Goblin == 0 && gameGrid[`tile${i}`].Player == 0){
-            tile.style.backgroundImage = 'url("sprites/cave_wall_breeze.png")'
+        const gGrid = gameGrid[`tile${i}`]
+        if(gGrid.Pit == 1){
+            gGrid.backgroundImage = 'url("sprites/cave_pit.png")'
+            
+        }else if(gGrid.Stench == 1 && gGrid.Breeze == 1 && gGrid.Goblin == 0 && gGrid.Player == 0){
+            gGrid.backgroundImage = 'url("sprites/cave_wall_stench_breeze.png")'
+
+        }else if(gGrid.Stench == 1 && gGrid.Goblin == 0 && gGrid.Player == 0){
+            gGrid.backgroundImage = 'url("sprites/cave_wall_stench.png")'
+
+        }else if(gGrid.Breeze == 1 && gGrid.Goblin == 0 && gGrid.Player == 0){
+            gGrid.backgroundImage = 'url("sprites/cave_wall_breeze.png")'
         }
         else{
-            tile.style.backgroundImage = 'url("sprites/cave_wall.png")'
+            gGrid.backgroundImage = 'url("sprites/cave_wall.png")'
         }
+
+        tile.style.backgroundImage = "none"
+        tile.style.backgroundColor = "grey"
         container.appendChild(tile);
-
-        if(gameGrid[`tile${i}`].Player == 1){
-            placeCharacter(i,1)
-        }
-
-        if(gameGrid[`tile${i}`].Goblin == 1){
-            placeCharacter(i,2)
-        }
-
     }
 }
 
